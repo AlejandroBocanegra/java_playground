dc5802a00a19d8bb3293a5cb8615a2c491e89c75
AlejandroBocanegra <a.boca@outlook.com>
2020-07-15 23:32:07 +0900

    Update javaplayground.java

--- src/javaplayground/javaplayground.java
+++ src/javaplayground/javaplayground.java
@@ -1,5 +1,41 @@
 public class javaplayground {
+
     public static void main(String[] args) {
-        System.out.println ("Hello World");
+        Manager manager = new Manager();
+        System.out.println(isEmployee(manager));
     }
+
+    //Returns true if the given object is an Employee object or a subclass of it.
+    static boolean isEmployee(Object o) {
+        return o instanceof Employee;
+    }
+        
+    static boolean isManager(Object o) {
+    //Returns true if the given object is a Manager object.
+        return o instanceof Manager;
+    }
+        
+    static boolean isCustomer(Object o) {
+    //Returns true if the given object is a Customer object.
+        return o instanceof Customer;
+    }
+        
+    static boolean isComparable(Object o) { 
+    //Returns true if the class of the given object implements the Comparable interface.
+        return o instanceof Comparable;
+    }
+
 }
+
+class User /*implements Comparable, Cloneable*/ {
+}
+
+class Employee extends User {
+}
+
+class Manager extends Employee {
+}
+
+class Customer extends User {
+}
+
